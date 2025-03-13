357ff8c881c2a074837e539a2b4e06b6f002fc0e
gopikaperiyasamy <gopika.al22@bitsathy.ac.in>
2025-03-13 22:56:55 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Medical-Chatbot-Generative-AI
+#Create & Activate the Environment
+#Once Conda is working, run:
+```bash 
+
+conda create -n medibot python=3.10 -y
+```
+```bash 
+conda activate medibot
+```
+#Then install dependencies:
+```bash 
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",  # Use lowercase and underscores for package names
+    version="0.0.0",  # Use valid semantic versioning
+    author="gopikaperiyasamy",
+    author_email="gopika.al22@bitsathy.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
